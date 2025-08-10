a4bc0ba57219cb80cecbb17a5d47d4c1e4f9f29e
ramraj24019 <81388110+ramraj24019@users.noreply.github.com>
2025-08-11 02:50:28 +0530

    Add files via upload

---
+++ crc/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import '../app.css';
+	import { Toasts } from '$lib/components/Toasts';
+	import GoogleAnalytics from '$lib/components/GoogleAnalytics.svelte';
+	
+	let { children } = $props();
+</script>
+
+<GoogleAnalytics />
+
+<nav class="container mx-auto navbar bg-base-100">
+	<div class="navbar-start">
+		<a class="normal-case text-xl" href="/">Teen Patti</a>
+	</div>
+	<div class="navbar-end">
+		<a
+			class="mr-4 link link-hover"
+			href="https://github.com/mustafa-kapadia1483/teen-patti"
+			target="_blank"
+			rel="noreferrer">Github</a
+		>
+		<a href="/create-room" class="btn btn-primary">Join / Create Room</a>
+	</div>
+</nav>
+
+<main class="container mx-auto px-2">
+	{@render children?.()}
+	<Toasts />
+</main>
