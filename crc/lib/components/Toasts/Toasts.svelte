6b41e78fbfc55e5bd294d7f8103b28e9980f2b1b
ramraj24019 <81388110+ramraj24019@users.noreply.github.com>
2025-08-11 02:34:32 +0530

    Add files via upload

---
+++ crc/lib/components/Toasts/Toasts.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { toastQueue } from './stores.svelte';
+</script>
+<div class="toast">
+    {#each toastQueue as { message, type }}
+        <div
+            class="alert"
+            class:alert-error={type === 'error'}
+            class:alert-info={type === 'info'}
+            class:alert-success={type === 'success'}
+        >
+            <div>
+                <span>{message}</span>
+            </div>
+        </div>
+    {/each}
+</div>
